e64bf69c32d206c4f9a9315b2f7ebbf15cd232f0
businan <yorulmazburaksinan@gmail.com>
2020-12-12 15:43:53 +0100

    saved user to DB

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,14 +1,28 @@
-exports.authRegister = (req, res) => {
+const User = require('../models/UserModel');
+
+exports.authRegister = async (req, res) => {
     //TODO: register function
     // req.body.'....'
 
     const {firstName, lastName, email, password } = req.body;
     console.log(firstName, lastName, email, password);
-    
+
     // TODO1: validate field
     // TODO2: Check already registered
     // TODO3: crpyt password
     // TODO4: save the user to DB
+
+
+    // TODO4: save the user to DB
+    const user = new User({
+        firstName,
+        lastName,
+        email,
+        password
+    })
+    await user.save();
+
+    // TODO: Error handling for saving
     res.send('Register Completed')
 }
 

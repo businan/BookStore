e5304e909486ea8f21c044db54a99d2a9b5af573
businan <yorulmazburaksinan@gmail.com>
2020-12-13 22:31:51 +0100

    package

--- server.js
+++ server.js
@@ -21,9 +21,8 @@ app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname,"client","build","index.html"))
 }) 
 };
-const hostVar = '0.0.0.0';
-const port = process.env.PORT || 5000;
 
-app.listen(port, hostVar, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+
+app.listen(process.env.port || 4000, () => {
+  console.log(`Server listening on port ${process.env.PORT}`)
+})

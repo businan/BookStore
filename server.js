c0177a66d21249036f6a01d73da6bc3e4dc837c0
businan <yorulmazburaksinan@gmail.com>
2020-12-14 23:05:48 +0100

    server.js

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const express = require('express');
+const app = express();
+
 require('dotenv').config();
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 const connectDB = require("./models/connectDB");
+const router = require('./routes/router');
+
 connectDB();
 
-const router = require('./routes/router');
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", router)
 
 
 // production
 if (process.env.NODE_ENV === "production") {
-   app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
-}) 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
 };
 
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log(`Server listening on port ${process.env.PORT}`)
-})
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`I'm listening on port ${port}`);
+});

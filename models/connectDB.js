eb3cb0f8fb7a4d408705c3b4131146c8ea76fbc8
businan <yorulmazburaksinan@gmail.com>
2020-12-14 18:40:07 +0100

    change db file

--- models/connectDB.js
+++ models/connectDB.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGODB_URI, { 
             useNewUrlParser: true, 
             useUnifiedTopology: true,
-            useFindAndModify: false,
+            useFindAndModify: true,
             useCreateIndex: true,
         });
         console.log('Succesfully connected to DB ');

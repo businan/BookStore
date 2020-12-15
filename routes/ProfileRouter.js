c027c3933f971ab475be955689769a51c80f4ffa
businan <yorulmazburaksinan@gmail.com>
2020-12-15 02:09:02 +0100

    change lowercase mistake

--- routes/ProfileRouter.js
+++ routes/ProfileRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const auth = require("../middleware/authMiddleware");
+const auth = require("../middleware/AuthMiddleware");
 const router = express.Router();
 const ProfileController = require("../controllers/ProfileController");
 
